194b6b4613069266f5baa92a9446eda804508515
Cauê Vitor <138812443+cauevitorr@users.noreply.github.com>
2024-05-13 17:11:32 -0300

    Add files via upload

---
+++ Styles/Card.js
@@ -0,0 +1,95 @@
+import styled from "styled-components"
+
+export const CardTop = styled.div`
+padding-top: 10px;
+width: 550px;
+height: 200px;
+background-color: white;
+border-top-left-radius: 10px;
+border-top-right-radius: 10px;
+`
+
+export const DivContainer = styled.div`
+display: flex;
+`
+export const Text01 = styled.h2`
+margin-left: 40px;
+color: hsl(179, 62%, 43%);
+`
+
+export const Text02 = styled.p`
+margin-left: 40px;
+color: hsl(71, 73%, 54%);
+`
+
+export const Text03 = styled.p`
+width: 500px;
+margin-left: 40px;
+color: hsl(218, 22%, 67%);
+`
+
+export const Text04 = styled.h3`
+color: white;
+margin-top: 40px;
+margin-left: 40px;
+`
+
+export const DivText = styled.div`
+display: flex;
+align-items: center;
+margin-left: 40px;
+`
+
+export const Text05 = styled.h1`
+color: white;
+`
+export const Text06 = styled.p`
+color: #DCDCDC;
+`
+export const Text07 = styled.p`
+color: white;
+margin-left: 30px;
+`
+
+export const Text08 = styled.h3`
+margin-top: 50px;
+color: white;
+margin-left: 40px;
+`
+
+export const Lista = styled.dl`
+color: #DCDCDC;
+`
+
+export const Button01 = styled.button`
+background-Color: hsl(71, 73%, 54%);
+color: white;
+border: none;
+border-radius: 5px;
+width: 200px;
+height: 30px;
+margin-left: 35px;
+cursor: pointer;
+&:hover{
+ color:  hsl(71, 73%, 54%);
+ background-color: white;
+}
+`
+
+
+
+export const  CardLeft = styled.div`
+width: 275px;
+height: 280px;
+background-color: hsl(179, 62%, 43%);
+border-bottom-left-radius: 10px;
+`
+
+export const  CardRight = styled.div`
+width: 275px;
+height: 280px;
+background-color: #66CDAA;
+border-bottom-right-radius: 10px;
+`
+
+
